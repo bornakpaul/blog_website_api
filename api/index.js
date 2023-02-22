782f5fb76bcf461da720697926e54a4366ee9233
bornakpaul <bornakpaul@gmail.com>
2023-02-22 21:14:25 +0530

    Code refactored

--- api/index.js
+++ api/index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 
 app.listen("3000", () =>{
-     console.log("Backend is running...........");
+     console.log("Backend is running.");
 });
