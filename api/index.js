db1f79a6c1e75698a8e19b6e72ca027af0a38baa
bornakpaul <bornakpaul@gmail.com>
2023-02-22 21:47:48 +0530

    Models created

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to mongoDB")).catch(err=> console.log(err));
 
 app.listen("3000", () =>{
      console.log("Backend is running.");
