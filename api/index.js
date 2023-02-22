85e735c7ea3a192568ac480d79d256364b6d93ea
bornakpaul <bornakpaul@gmail.com>
2023-02-22 23:46:47 +0530

    Register api completed

--- api/index.js
+++ api/index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to mongoDB")).catch(err=> console.log(err));
+mongoose.connect(process.env.MONGO_URL).then(
+     console.log("Connected to mongoDB")
+     ).catch(err=> console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("3000", () =>{
      console.log("Backend is running.");
