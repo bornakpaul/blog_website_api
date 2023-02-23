9cbd52b768b05ba014e0e82285f0093408538a3b
bornakpaul <bornakpaul@gmail.com>
2023-02-23 23:52:00 +0530

    UPDATE users

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,13 +5,20 @@ const bcrypt = require('bcrypt');
 // UPDATE
 router.put("/:id", async(req,res) => {
      if(req.body.userId === req.params.id){
-          try{
-
-          }catch(err){
+          if(req.body.password){
+               const salt =  await bcrypt.genSalt(10);
+               req.body.password = await bcrypt.hash(req.body.password, salt);
+          }
+          try {
+              const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+               $set: req.body,
+              },{new: true});
+              res.status(200).json(updatedUser);
+          } catch(err) {
                res.status(500).json(err);
           }
      } else {
-          res.status(401).json("You can update only your account"); 
+          res.status(401).json("You can update only your account");
      }
 });
 
