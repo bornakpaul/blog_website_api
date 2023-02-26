fd2466c5933919dcfb081bf264acb70b1e41a64e
bornakpaul <bornakpaul@gmail.com>
2023-02-26 23:34:27 +0530

    POST crud completed

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -38,13 +38,56 @@ router.put("/:id", async(req,res) => {
 });
 
 // DELETE POST
-router.delete("/:id", async(req, res) => {
-     
+router.delete("/:id", async(req,res) => {
+     try{
+          const post = await Post.findById(req.params.id);
+          if(post.username === req.body.username){
+               try{
+               await post.delete();
+               res.status(200).json("Post has been deleted.");
+               } catch(err){
+                    res.status(500).json(err);
+               }     
+          }else{
+               res.status(401).json("You can delete only your posts.")
+          } 
+     }catch(err){
+          res.status(500).json(err);
+     }
 });
 
-// GET POST
-router.get("", async(req, res) => {
+// GET POST BY USER
+router.get("/:id", async(req, res) => {
+     try{
+          const post = await Post.findById(req.params.id);
+          res.status(200).json(post);
+     }catch(err){
+          res.status(500).json(err);
+     }
+});
 
+// GET ALL POSTS
+// if user is passed in the url display posts of that user
+// if category is passed in the url display posts of that category
+// else display all posts
+router.get("/", async(req, res) => {
+     const username = req.query.user;
+     const catName = req.query.category;
+     try{
+          let posts;
+          if(username){
+               posts = await Post.find({username});
+          }else if(catName){
+               posts = await Post.find({categories:{
+                    $in:[catName]
+               }});
+          }else{
+               posts = await Post.find();
+          }
+          res.status(200).json(posts);
+     }catch(err){
+          res.status(500).json(err);
+     }
 });
 
 module.exports = router
